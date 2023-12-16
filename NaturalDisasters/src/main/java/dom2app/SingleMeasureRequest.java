bfab78a3926761db2b72aaa1284cbf7f89ce075b
Zodimord <cs04937@uoi.gr>
2023-12-16 22:23:55 +0200

    bruh

---
+++ NaturalDisasters/src/main/java/dom2app/SingleMeasureRequest.java
@@ -0,0 +1,10 @@
+package dom2app;
+
+import java.util.List;
+import java.util.Collections;
+import org.apache.commons.math3.util.Pair;
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+
+public class SingleMeasureRequest implements ISingleMeasureRequest {
+
+}
